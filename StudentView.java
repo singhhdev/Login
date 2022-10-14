058c1d47ef2e2d0703c67a9c1f9480e8867820fd
Karanpreet Singh <108330780+karanbedii@users.noreply.github.com>
2022-10-14 04:14:44 -0700

    Update StudentView.java

--- StudentView.java
+++ StudentView.java
@@ -1,5 +1,30 @@
 package Project;
 
-public class StudentView {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class StudentView {
+	JFrame frame;
+	JButton orderHere;
+	JButton checkStatus;
+	JLabel actorName;
+	
+	public void setUpButtons() {
+		frame = new JFrame();
+		orderHere = new JButton();
+		checkStatus = new JButton();
+	}
+	public void setUpScene(String actor) {
+		frame.setLayout(null);
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+		frame.setTitle("Welcome || " + actor);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		
+	}
+	StudentView(String actor){
+		setUpButtons();
+		setUpScene(actor);
+	}
 }
